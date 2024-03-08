8842ccba8da57fa80624a44d7e9ff87ada1b5adf
Chris-oduro <bluebird23szn@gmail.com>
2024-03-08 10:30:19 +0000

    redo

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import logo1 from "../assets/2nd.png"
+import Navbar from "./Navbar"
+
+const Header = () => {
+  return (
+    <header className='header '>
+        
+            <nav className="nav  flex justify-between ">
+            <img src={logo1} alt=""  className='w-[10rem] h-[3rem] mt-[4px] ml-4'/>
+            <Navbar />
+            </nav>
+    </header>
+  )
+}
+
+export default Header
