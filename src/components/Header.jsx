64f25b00cbccd4ee38c3aef3a7f597345ddf6d6a
Christian <Bluebird23szn@gmail.com>
2024-03-14 23:44:28 -0700

    enhanced

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,11 @@ import Navbar from "./Navbar"
 
 const Header = () => {
   return (
-    <header className='header '>
+    <header className='header bg-white border-none'>
         
             <nav className="nav  flex justify-between ">
             <img src={logo1} alt=""  className='w-[10rem] h-[3rem] mt-[4px] ml-4'/>
-            <Navbar />
+            <Navbar/>
             </nav>
     </header>
   )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,28 +1,61 @@
-import React from 'react'
-import Header from '../components/Header'
-import aboutimg from "../assets/about.jpg"
-import AlternateTimeline from '../constants/Timeline'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { FullPage, Slide } from 'react-full-page';
+import Header from '../components/Header';
 
 const About = () => {
   return (
-    <div className=''>
-        <Header/>
-      <div className=' mt-[2.5rem] flex justify-center'>
-            <img src={aboutimg} alt="" className='w-[20rem] h-[20rem]' />
-      </div>
-      <div className='mx-[3.5rem] mt-[2rem] text-black font-bold text-2xl'>
-        Hello, I'm Amin Yakubu
-      </div>
-<p className=' mx-[3.5rem]'>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium voluptatum saepe aliquid dolore magni a quisquam nulla, ducimus earum  doloremque quasi voluptate aliquam similique ratione tenetur porro consectetur commodi. Voluptates.
-</p>
-
-<div className='mt-[2rem]'>
-    <AlternateTimeline />
-</div>
-
+    <>
+    <div className='fixed w-screen z-30'>
+    <Header/>
     </div>
-  )
+    <FullPage>
+      <Slide>
+        <div className="bg-white w-screen h-screen flex flex-1 flex-col justify-center items-center">
+          
+          <motion.div 
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="text-4xl font-bold text-gray-800 mb-8 flex justify-center items-center"
+          >
+            Introduction
+          </motion.div>
+          <motion.div 
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 1 }}
+            className="text-lg text-gray-700 max-w-lg text-center"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed sit amet elit at ligula porttitor consequat. Duis laoreet nisi in ex scelerisque feugiat.
+          </motion.div>
+        </div>
+      </Slide>
+      <Slide>
+        <div className="bg-white w-screen h-screen flex flex-col justify-center items-center">
+          
+          <motion.div 
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="text-4xl font-bold text-gray-800 mb-8"
+          >
+            Our Story Begins
+          </motion.div>
+          <motion.div 
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 1 }}
+            className="text-lg text-gray-700 max-w-lg text-center"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed sit amet elit at ligula porttitor consequat. Duis laoreet nisi in ex scelerisque feugiat.
+          </motion.div>
+        </div>
+      </Slide>
+      {/* Add more slides as needed */}
+    </FullPage>
+    </>
+  );
 }
 
-export default About
+export default About;
